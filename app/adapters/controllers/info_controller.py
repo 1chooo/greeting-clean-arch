d1a43f0c388ee4f3f8af68b55ae535c296cadba0
1chooo <hugo970217@gmail.com>
2024-04-22 10:03:04 +0800

    feat: error handle in controller

--- app/adapters/controllers/info_controller.py
+++ app/adapters/controllers/info_controller.py
@@ -1,4 +1,5 @@
 from fastapi import APIRouter, status
+from fastapi.exceptions import HTTPException
 
 from app.config import app_settings
 
@@ -6,14 +7,20 @@ info_routes = APIRouter(tags=["info"], prefix="/info")
 
 @info_routes.get("/", status_code=status.HTTP_200_OK)
 def get_infor():
-    return {
-        "app_name"  : app_settings.app_name,
-        "author"    : app_settings.author,
-        "app_mode"  : app_settings.app_mode,
-        "host"      : app_settings.host,
-        "port"      : app_settings.port,
-        "reload"    : app_settings.reload,
-        "redis_host": app_settings.redis_host,
-        "redis_port": app_settings.redis_port
-    }
+    try:
+        return {
+            "app_name"  : app_settings.app_name,
+            "author"    : app_settings.author,
+            "app_mode"  : app_settings.app_mode,
+            "host"      : app_settings.host,
+            "port"      : app_settings.port,
+            "reload"    : app_settings.reload,
+            "redis_host": app_settings.redis_host,
+            "redis_port": app_settings.redis_port
+        }
+    except Exception as e:
+        raise HTTPException(
+            status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, 
+            detail=str(e)
+        )
 
